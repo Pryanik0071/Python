f839f77e577ae551b814ccb6d74c9e632bd407a8
Dmitry <di.mesler.2010@yandex.ru>
2022-12-20 23:49:37 +0300

    Add: rgb to hex

--- python/bin_operations/bin.py
+++ python/bin_operations/bin.py
@@ -2,3 +2,8 @@
 def to_bin(number, count):
     """Return bin number with count left filled '0' digits"""
     return format(number, 'b').zfill(count)
+
+
+# Число из 2 в 16
+def rgb2hex(r=None, g=None, b=None):
+    return f'#{r:02x}{g:02x}{b:02x}'
