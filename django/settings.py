cc34ceb06de24f5633021b9deba2356017017523
Dmitry M. <di.mesler.2010@yandex.ru>
2024-12-05 16:43:00 +0300

    Update: Django

---
+++ django/settings.py
@@ -0,0 +1,10 @@
+# Прописываем в Settings load_env()
+# import os
+#
+# from dotenv import load_dotenv  # Импортируем environ
+#
+# load_dotenv()  # Загрузка переменных окружения из файла .env
+#
+#
+# SECRET_KEY = os.getenv('SECRET_KEY')
+# DEBUG = os.getenv('DEBUG', False)
